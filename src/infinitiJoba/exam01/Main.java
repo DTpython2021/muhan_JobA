8cfaad44dfab31086dc992d40f949df13ee0efd3
BMT <namou361@gmail.com>
2020-07-20 13:54:22 +0900

    //test

--- src/infinitiJoba/exam01/Main.java
+++ src/infinitiJoba/exam01/Main.java
@@ -5,7 +5,11 @@ public class Main {
 	public static final int SCREEN_HEIGHT = 720;
 	
 	public static void main(String[] args) {
-		new GamePaly();		
+		new GamePaly();	
+		
+		
+		//test
+		
 	}
 
 }
